3aef4048c773b20553d4420d8d87ffc3a1dc4290
Lukas Santos Venancio <lukassvenancio022@gmail.com>
2022-01-13 13:34:56 -0300

    Exercise 5 completed.

---
+++ Exercise05/exercise05.py
@@ -0,0 +1,5 @@
+number = int(input('Enter any number: '))
+predecessor = number - 1
+successor = number + 1
+
+print('The predecessor of {} is {} and the successor is {}. '.format(number, predecessor, successor))
