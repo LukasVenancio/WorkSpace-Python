2f00ba8fb502ed912bf57416487a4942ad169d62
Lukas Santos Venancio <lukassvenancio022@gmail.com>
2022-01-13 13:49:25 -0300

    Exercise 6 completed.

---
+++ Exercise06/exercise06.py
@@ -0,0 +1,7 @@
+number = int(input('Enter any number: '))
+double = number * 2
+triple = number * 3
+squareRoot = number ** 0.5
+
+print(
+    'The double of {} is {}, your triple is {} and the square root is {}. '.format(number, double, triple, squareRoot))
