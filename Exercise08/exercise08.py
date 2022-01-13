e82d0bfffbf9132267a81d8c1102ba6995b4bc1d
Lukas Santos Venancio <lukassvenancio022@gmail.com>
2022-01-13 14:49:39 -0300

    Exercise 8 completed.

---
+++ Exercise08/exercise08.py
@@ -0,0 +1,7 @@
+valueInMeters = int(input('Type the value in meters: '))
+valueInCentimeters = valueInMeters * 100
+valueInMillimeters = valueInMeters * 1000
+
+print('Value in meters: {}m'.format(valueInMeters))
+print('Value in centimeters: {}cm'.format(valueInCentimeters))
+print('Value in millimeters: {}mm'.format(valueInMillimeters))
